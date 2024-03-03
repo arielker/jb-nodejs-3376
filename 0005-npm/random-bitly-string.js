b5aa5206cec1b231e66823673066243a6e65a509
unknown <arielk2356@gmail.com>
2024-03-03 16:48:58 +0200

    class3+4

--- 0005-npm/random-bitly-string.js
+++ 0005-npm/random-bitly-string.js
@@ -5,6 +5,21 @@ for(i=0; i < 5; i++) {
     const rand = Math.round(Math.random() * letters.length);
     ret += letters[rand];
 }
-console.log(ret);
+// console.log(ret);
 
 
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.round(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+for (let index = 0; index < 8; index++) {
+    console.log(makeid(5));   
+}
