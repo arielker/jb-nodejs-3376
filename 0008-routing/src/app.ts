1c4f720fe7e3434c2a70a4219d7c41ed7ec0c6e6
unknown <arielk2356@gmail.com>
2024-03-06 09:00:11 +0200

    additions from yesterday's missed class

--- 0008-routing/src/app.ts
+++ 0008-routing/src/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import basicRouter from './routers/basic-router';
 import routerWithMiddlewares from './routers/router-with-middlewares'
 
-const server = express();
+const server: string = express();
 
 server.use('/basic-router', basicRouter);
 server.use('/router-with-middlewares', routerWithMiddlewares);
 
-server.listen(8080);
+server.listen(8080, () => {
+    console.log('started...')
+});
